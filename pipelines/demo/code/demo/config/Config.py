3f4ab97c54cb76b510a4e06c2a1165ac835d5f49
vlaunir@prophecy.io <vlaunir@prophecy.io>
2024-07-10 18:44:03 +0000

    Modifies job_fin_crimes to include new pipeline components and updates demo pipeline configuration.

--- pipelines/demo/code/demo/config/Config.py
+++ pipelines/demo/code/demo/config/Config.py
@@ -1,19 +1,12 @@
-from demo.graph.risk_scoring.config.Config import SubgraphConfig as risk_scoring_Config
 from prophecy.config import ConfigBase
 
 
 class Config(ConfigBase):
 
-    def __init__(self, risk_scoring: dict=None, **kwargs):
+    def __init__(self, **kwargs):
         self.spark = None
-        self.update(risk_scoring)
+        self.update()
 
-    def update(self, risk_scoring: dict={}, **kwargs):
+    def update(self, **kwargs):
         prophecy_spark = self.spark
-        self.risk_scoring = self.get_config_object(
-            prophecy_spark, 
-            risk_scoring_Config(prophecy_spark = prophecy_spark), 
-            risk_scoring, 
-            risk_scoring_Config
-        )
         pass
--- pipelines/demo/code/demo/pipeline.py
+++ pipelines/demo/code/demo/pipeline.py
@@ -4,23 +4,9 @@ from pyspark.sql.types import *
 from demo.config.ConfigStore import *
 from demo.udfs.UDFs import *
 from prophecy.utils import *
-from demo.graph import *
 
 def pipeline(spark: SparkSession) -> None:
-    df_ds_country_watchlist = ds_country_watchlist(spark)
-    df_all_scored_transx = all_scored_transx(spark)
-    df_ds_person_watchlist = ds_person_watchlist(spark)
-    df_transaction_details = transaction_details(
-        spark, 
-        df_all_scored_transx, 
-        df_ds_country_watchlist, 
-        df_ds_person_watchlist
-    )
-    df_transaction_details_1 = transaction_details_1(spark, df_transaction_details)
-    df_Reformat_1 = Reformat_1(spark, df_transaction_details)
-    df_group_by_origin_dest = group_by_origin_dest(spark, df_transaction_details_1)
-    df_limit_10 = limit_10(spark, df_group_by_origin_dest)
-    df_risk_scoring_out, df_risk_scoring_out0 = risk_scoring(spark, Config.risk_scoring, df_transaction_details)
+    pass
 
 def main():
     spark = SparkSession.builder\
